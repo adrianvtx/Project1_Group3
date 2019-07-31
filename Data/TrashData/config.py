bd681810b7a49285a0d3e4bfe8e3c904242d8c5e
adrianvtx <adrian.voj.2019@gmail.com>
2019-07-30 20:12:57 -0500

    Delete config.py

--- Data/TrashData/config.py
+++
@@ -1,7 +0,0 @@
-# Enter your API key
-gkey = "AIzaSyDh0jhkQC5_fBAga4qkDHkQMuJRAIFx35k"
-gkey2 = "AIzaSyBVlsH3ugUOiaJNsHpreUDtjS4Dhha0U0o"
-# Enter your Google Application Credentials PATH
-GOOGLE_APPLICATION_CREDENTIALS = "D:\D DOWNLOADs\DOCUMENTATION\GoogleCloudServices\Analytics Bootcamp Rice U-e975bbd03fde.json"
-
-
